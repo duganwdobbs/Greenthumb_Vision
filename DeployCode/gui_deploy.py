7829f1d667dab48ae7b56c08371d5e23018ddf3d
Juanald MacDonald <checkjuan23@gmail.com>
2018-04-29 11:59:00 -0500

    Update gui_deploy.py

--- DeployCode/gui_deploy.py
+++ DeployCode/gui_deploy.py
@@ -135,7 +135,7 @@ if __name__ == '__main__':
 
   # setup the update callback
   master.after(0, func=lambda: update_all(master, image_label, cam, fps_label, net))
-  master.lift()
-  master.attributes('-topmost', True)
-  master.after_idle(root.attributes,'-topmost', False)
+  root.lift()
+  root.attributes('-topmost', True)
+  root.after_idle(root.attributes,'-topmost', False)
   master.mainloop()
